7c418df2184c32c3a321ff97175524c2cbe25661
Betsy Gillard <beegillard@gmail.com>
2022-06-24 12:17:30 -0600

    Complete JavaScript portion

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-console.log('this is mixed messages');
+const first = [
+    'Today is your day',
+'Today is not your day',
+'The time is now',
+'Tomorrow will be your day'
+];
+
+const second = [
+    'to go cause some trouble',
+'to take some time for introspection',
+'to give BirdDog some treats',
+'to eat a lot of cookies',
+];
+
+const third = [
+'the difference between "they\'re" "there" and "their"',
+'capitalism is a death cult',
+'NO FORCED PREGNANCIES. TRUST WOMEN.',
+'I just took a DNA test turns out I\'m 100% that bitch',
+];
+
+const generateSentence = () => {
+    let one = first[Math.floor(Math.random() * 4)];
+    let two = second[Math.floor(Math.random() * 4)];
+    let three = third[Math.floor(Math.random() * 4)];
+
+    return `${one} ${two}, but don't forget ${three}`;
+
+}
+
+console.log(generateSentence());
